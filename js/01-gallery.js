ee895d86cebe19672f1b6b901e1a94b047045d71
davidfsora <davidsora0204@gmail.com>
2023-07-05 23:54:43 -0500

    Primera subida tarea 7 js

---
+++ js/01-gallery.js
@@ -0,0 +1,59 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+let head = document.querySelector("head");
+let basicLightBoxCss = document.createElement("link");
+basicLightBoxCss.rel = "stylesheet";
+basicLightBoxCss.href = "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css";
+head.appendChild(basicLightBoxCss);
+
+let body = document.querySelector("body");
+let basicLightBoxScript = document.createElement("script");
+basicLightBoxScript.src = "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
+body.appendChild(basicLightBoxScript);
+
+let gallery = document.querySelector(".gallery");
+
+for (let index = 0; index < galleryItems.length; index++) {
+    let galleryItem = document.createElement("li");
+    galleryItem.classList.add("gallery__item");
+    gallery.appendChild(galleryItem);
+
+    let galleryLink = document.createElement("a");
+    galleryLink.href = galleryItems[index].original;
+    galleryLink.classList.add("gallery__link");
+    galleryItem.appendChild(galleryLink);
+
+    let galleryImage = document.createElement("img");
+    galleryImage.classList.add("gallery__image");
+    galleryImage.src = galleryItems[index].preview;
+    galleryImage.dataset.source = galleryItems[index].original;
+    galleryImage.alt = galleryItems[index].description;
+    galleryLink.appendChild(galleryImage);   
+}
+
+let lightboxInstance;
+
+let images = document.querySelectorAll(".gallery__link");
+images.forEach((image) => {
+    image.addEventListener("click", (event) => {
+    let imageLink = image.getAttribute("href");
+    event.preventDefault();
+
+    lightboxInstance = basicLightbox.create(`
+        <img width="100%" height="100%" src="${imageLink}">`,
+    {
+        closable: false
+    });
+
+    lightboxInstance.show();
+    });
+
+    image.addEventListener("keydown", (event) => {
+    event.preventDefault();
+    if (event.keyCode === 27 && lightboxInstance) {
+        lightboxInstance.close();
+    }
+    });
+});
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,80 @@
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+// let head = document.querySelector("head");
+// let simpleLightBoxCss = document.createElement("link");
+// simpleLightBoxCss.rel = "stylesheet";
+// simpleLightBoxCss.href = "https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.14.1/simple-lightbox.css";
+// head.appendChild(simpleLightBoxCss);
+
+// let body = document.querySelector("body");
+// let simpleLightBoxScript = document.createElement("script");
+// simpleLightBoxScript.src = "https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.14.1/simple-lightbox.min.js";
+// body.appendChild(simpleLightBoxScript);
+
+// let gallery = document.querySelector(".gallery");
+
+// for (let index = 0; index < galleryItems.length; index++) {
+//     let galleryItem = document.createElement("li");
+//     galleryItem.classList.add("gallery__item");
+//     gallery.appendChild(galleryItem);
+
+//     let galleryLink = document.createElement("a");
+//     galleryLink.href = galleryItems[index].original;
+//     galleryLink.classList.add("gallery__link");
+//     galleryItem.appendChild(galleryLink);
+
+//     let galleryImage = document.createElement("img");
+//     galleryImage.classList.add("gallery__image");
+//     galleryImage.src = galleryItems[index].preview;
+//     galleryImage.dataset.source = galleryItems[index].original;
+//     galleryImage.alt = galleryItems[index].description;
+//     galleryLink.appendChild(galleryImage);   
+// }
+
+// let lightbox = new SimpleLightbox('.gallery a', {
+//     captionsData : 'alt',
+//     captionDelay : 250
+// });
+
+import { galleryItems } from './gallery-items.js';
+
+let head = document.querySelector('head');
+let simpleLightBoxCss = document.createElement('link');
+simpleLightBoxCss.rel = 'stylesheet';
+simpleLightBoxCss.href = 'https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.14.1/simple-lightbox.css';
+head.appendChild(simpleLightBoxCss);
+
+let body = document.querySelector('body');
+let simpleLightBoxScript = document.createElement('script');
+simpleLightBoxScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.14.1/simple-lightbox.min.js';
+
+simpleLightBoxScript.onload = function () {
+  let gallery = document.querySelector('.gallery');
+
+  for (let index = 0; index < galleryItems.length; index++) {
+    let galleryItem = document.createElement('li');
+    galleryItem.classList.add('gallery__item');
+    gallery.appendChild(galleryItem);
+
+    let galleryLink = document.createElement('a');
+    galleryLink.href = galleryItems[index].original;
+    galleryLink.classList.add('gallery__link');
+    galleryItem.appendChild(galleryLink);
+
+    let galleryImage = document.createElement('img');
+    galleryImage.classList.add('gallery__image');
+    galleryImage.src = galleryItems[index].preview;
+    galleryImage.dataset.source = galleryItems[index].original;
+    galleryImage.alt = galleryItems[index].description;
+    galleryLink.appendChild(galleryImage);
+  }
+
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+  });
+};
+
+body.appendChild(simpleLightBoxScript);
+
